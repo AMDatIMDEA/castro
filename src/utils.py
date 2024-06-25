9c2fe6c89f22d4c65064a21b01eeeb00f9ec4b40
schenkch <0christina.schenk0@gmail.com>
2024-06-25 15:50:07 +0200

    updated docs

--- src/utils.py
+++ src/utils.py
@@ -4,19 +4,18 @@
 # author: Christina Schenk
 
 def save_to_csv(filepath, samples):
-        """
+    """
     writes samples to csv file
-        
+
     Parameters
     ----------
-    filepath: file path e.g. Path('Outputs/LHS_with_new_permutations_correct_suggestions_allselected_imp_ext_subprobs_improve.csv')  
+    filepath: file path e.g. Path('Outputs/LHS_with_new_permutations_correct_suggestions_allselected_imp_ext_subprobs_improve.csv')
     samples: pandas dataframe with samples nsamp x ncomponents
-    
+
     Returns
     -------
-    csv file 
-    
+    csv file
+
     """
-    filepath.parent.mkdir(parents=True, exist_ok=True)  
-    samples.to_csv(filepath) 
-    
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    samples.to_csv(filepath)
