a9f74d1d7a0ee7dd5386d7694c5376b972afdcca
schenkch <0christina.schenk0@gmail.com>
2024-07-08 13:28:56 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup
+
+import os.path
+import os
+from setuptools import dist
+
+
+readme = ''
+here = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(here, 'README.md')
+if os.path.exists(readme_path):
+    with open(readme_path, 'rb') as stream:
+        readme = stream.read().decode('utf8')
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    long_description=readme,
+    name='castro',
+    version='1.0',
+    description='CASTRO - Constrained sequential Latin hypercube (with multidimensional uniformity) sampling',
+    python_requires='>=3.9',
+    project_urls={
+        "repository": "https://github.com/AMDatIMDEA/castro"},
+    author='Christina Schenk',
+    author_email='christina.schenk@imdea.org',
+    maintainer='Christina Schenk',
+    license='GPL V3',
+    keywords='Constrained sequential Latin hypercube (with multidimensional uniformity) sampling',
+    packages=[
+        'castro'
+    ],
+    install_requires=required,
+    #extras_require={"dev": ["pytest==5.*,>=5.2.0"]},
+
+)
