fe2b3f8f9084f093db35e58cd74bc6126b9f2d09
schenkch <0christina.schenk0@gmail.com>
2024-07-09 11:02:21 +0200

    check docs

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     ],
     extras_require={
         'tests': ['pytest', 'codecov', 'pytest-cov'],
-        'docs': ['sphinx', 'sphinx-build', 'sphinx-rtd-theme', 'myst-parser', 'myst-nb', 'sphinx-panels', 'autodocs'],
+        'docs': ['sphinx', 'sphinx-rtd-theme', 'myst-parser', 'myst-nb', 'sphinx-panels', 'autodocs'],
         'python_version == "3.9"': [
             'ipython>8.13, <8.19',
         ],
