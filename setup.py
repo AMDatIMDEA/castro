e8458653a5606cf73ad93d5e98d4e986d2ad2f7d
schenkch <0christina.schenk0@gmail.com>
2024-07-09 15:43:27 +0200

    check docs

--- setup.py
+++ setup.py
@@ -103,18 +103,21 @@ setup(
     extras_require={
         'tests': ['pytest', 'codecov', 'pytest-cov'],
         'docs': [
+            'sphinx==7.3.7',
+            'sphinx-rtd-theme==2.0.0',
+            'myst-parser',
+            'myst-nb',
+            'sphinx-panels',
             'sphinxcontrib.devhelp==1.0.5',
             'sphinxcontrib.applehelp==1.0.2',
             'sphinxcontrib.htmlhelp==2.0.4',
             'sphinxcontrib.serializinghtml==1.1.9',
             'sphinxcontrib.qthelp==1.0.6',
             'docutils<0.21',
-            'sphinx-rtd-theme==2.0.0',
             'sphinxcontrib.viewcode',
             'sphinx.ext.napoleon',
             'sphinxcontrib.mathjax',
             'sphinxcontrib.jquery==4.0',
-            'sphinx==7.3.7',
             'autodocs'
         ],
     },
