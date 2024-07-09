dea99fd72ed9b5457c8e2fba20a291c18dfead2e
schenkch <0christina.schenk0@gmail.com>
2024-07-09 15:40:10 +0200

    check docs

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx.ext.jquery',
+    'sphinxcontrib.jquery',
     'sphinx.ext.mathjax'
 ]
 
