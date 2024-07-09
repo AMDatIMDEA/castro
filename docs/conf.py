8a5ab77564bfc5b7dda931f735e6e9abf19b3659
schenkch <0christina.schenk0@gmail.com>
2024-07-09 14:06:50 +0200

    check docs

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ html_static_path = ['_static']
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxc.ext.applehelp',
+    'sphinx.contrib.applehelp',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
